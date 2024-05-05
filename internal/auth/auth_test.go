3db9fe12ad024bd0fd98dc17dc603d18a792f3e5
Deluroth <gwelwenant@protonmail.com>
2024-05-05 17:23:28 +0200

    test ci with failing test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,54 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name          string
+		headers       http.Header
+		expectedKey   string
+		expectedError errora
+	}{
+		{
+			name:          "No Authorization Header",
+			headers:       http.Header{},
+			expectedKey:   "",
+			expectedError: ErrNoAuthHeaderIncluded,
+		},
+		{
+			name: "Malformed Authorization Header",
+			headers: http.Header{
+				"Authorization": []string{"MalformedHeader"},
+			},
+			expectedKey:   "",
+			expectedError: errors.New("malformed authorization header"),
+		},
+		{
+			name: "Successful API Key Retrieval",
+			headers: http.Header{
+				"Authorization": []string{"ApiKey 12345"},
+			},
+			expectedKey:   "12345",
+			expectedError: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			key, err := GetAPIKey(tt.headers)
+			if key != tt.expectedKey {
+				t.Errorf("expected %s, got %s", tt.expectedKey, key)
+			}
+			if err != nil && err.Error() != tt.expectedError.Error() {
+				t.Errorf("expected error %s, got %s", tt.expectedError, err)
+			}
+			if err == nil && tt.expectedError != nil {
+				t.Errorf("expected error %s, got none", tt.expectedError)
+			}
+		})
+	}
+}
