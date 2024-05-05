2cdb2ff51a1657ec9208b9b3a29bb8ccc49a4cc7
Deluroth <gwelwenant@protonmail.com>
2024-05-05 18:09:50 +0200

    fix gosec security issues

--- json.go
+++ json.go
@@ -19,13 +19,16 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	dat, err := json.Marshal(payload)
-	if err != nil {
-		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader(500)
-		return
-	}
-	w.WriteHeader(code)
-	w.Write(dat)
+    w.Header().Set("Content-Type", "application/json")
+    dat, err := json.Marshal(payload)
+    if err != nil {
+        log.Printf("Error marshalling JSON: %s", err)
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
+    w.WriteHeader(code)
+    if _, err := w.Write(dat); err != nil {
+        log.Printf("Error writing JSON: %s", err)
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+    }
 }
